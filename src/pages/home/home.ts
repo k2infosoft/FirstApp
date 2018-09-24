8baa6e97fad1a385278c8b14afc25c95a2654bd9
MacBook <MacBook@Maples-MacBook-Air.local>
2018-09-25 03:45:29 +0530

    comit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheet, ActionSheetOptions } from '@ionic-native/action-sheet'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private actionSheet : ActionSheet,public navCtrl: NavController) {}
 
+  async showActionSheet(){
+    try {
+      const options : ActionSheetOptions = {
+        title :'My Action Sheet',
+        subtitle : 'Select an Option',
+        buttonLabels : ['Share on Facebook','Share on Twitter','Share in Linkedin'],
+        addCancelButtonWithLabel : 'Cancel',
+        addDestructiveButtonWithLabel : 'Delete',
+        androidTheme: 1,
+        destructiveButtonLast :true
+      }
+      const  selectedButtonIndex = await this.actionSheet.show(options)
+
+      console.log(`User selected ${selectedButtonIndex}`)
+      
+    } catch (e) {
+      console.error(e)
+    }
   }
-
+  
 }
